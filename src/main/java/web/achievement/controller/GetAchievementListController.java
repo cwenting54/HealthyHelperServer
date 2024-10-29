9e9f3f0418080946ba8bcb52f49a095154d9532f
Sophie <cwenting54@gmail.com>
2024-10-29 15:35:10 +0800

    revise achievementList

--- src/main/java/web/achievement/controller/GetAchievementListController.java
+++ src/main/java/web/achievement/controller/GetAchievementListController.java
@@ -25,55 +25,6 @@ import web.achievement.dao.impl.AchievementDaoImpl;
 import web.achievement.vo.Achievement;
 
 
-@WebServlet("/achievement/getlist")
-public class GetAchievementListController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private AchievementDao achievementDao;
-
-	@Override
-	public void init() throws ServletException {
-		try {
-			achievementDao = new AchievementDaoImpl();
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("application/json");
-	    resp.setCharacterEncoding("UTF-8");
-
-	    List<Achievement> achievements = achievementDao.selectAchievementsByUserId(2);
-
-	    Gson gson = new Gson();
-
-	    JsonObject response = new JsonObject();
-	    JsonArray achievementsArray = new JsonArray();
-
-	    for (Achievement achievement : achievements) {
-	        JsonObject achievementJson = new JsonObject();
-	        achievementJson.addProperty("aname", achievement.getAname());
-	        achievementJson.addProperty("aTypeId", achievement.getaTypeId());
-	        achievementJson.addProperty("content", achievement.getContent());
-
-	        achievementJson.addProperty("finishtime",
-	            achievement.getFinishtime() != null ? 
-	            new SimpleDateFormat("yyyy-MM-dd").format(achievement.getFinishtime()) : null);
-
-	        achievementJson.addProperty("photo", 
-	            achievement.getPhoto() != null ? 
-	            Base64.getEncoder().encodeToString(achievement.getPhoto()) : null);
-
-	        achievementsArray.add(achievementJson);
-	    }
-
-	    response.add("achievements", achievementsArray);
-	    
-	    PrintWriter out = resp.getWriter();
-	    out.println(gson.toJson(response));
-	}
-=======
 
 @WebServlet("/achievement/getlist")
 public class GetAchievementListController extends HttpServlet {
@@ -116,5 +67,5 @@ public class GetAchievementListController extends HttpServlet {
 		}
     }
 	
->>>>>>> 6ebef94161bc9e54ec41cde1ebe43ef3ffb89e4b
+
 }
