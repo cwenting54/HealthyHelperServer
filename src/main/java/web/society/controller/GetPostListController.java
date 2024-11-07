3a53d41f2a78453417f7d663ecdeff9773ff19be
Yu-Ting <a22627046@gmail.com>
2024-11-07 15:23:09 +0800

    modify GetPostListController

--- src/main/java/web/society/controller/GetPostListController.java
+++ src/main/java/web/society/controller/GetPostListController.java
@@ -2,6 +2,9 @@ package web.society.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.List;
 
 import javax.naming.NamingException;
@@ -12,6 +15,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import web.society.service.PostService;
 import web.society.service.impl.PostServiceImpl;
@@ -20,6 +25,7 @@ import web.society.vo.Post;
 @WebServlet("/post")
 public class GetPostListController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final static String CONTENT_TYPE = "application/json; charset=UTF-8";
 	private PostService postService;
 @Override
     public void init() throws ServletException {
@@ -29,20 +35,53 @@ public class GetPostListController extends HttpServlet {
 		e.printStackTrace();
 	}
     }
+//@Override
+//protected void doPost(HttpServletRequest request, HttpServletResponse response)
+//        throws ServletException, IOException {
+//    response.setContentType("application/json; charset=UTF-8");
+//
+//    // 使用 Service 層取得所有貼文
+//    List<Post> posts = postService.selectPost();
+//
+//    // 使用 Gson 將資料轉換成 JSON 格式
+//    Gson gson = new Gson();
+//    String json = gson.toJson(posts);
+//
+//    // 將 JSON 回傳到回應中
+//    response.getWriter().write(json);
+//}
+
 @Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-    response.setContentType("application/json; charset=UTF-8");
+protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	resp.setContentType(CONTENT_TYPE);
+	resp.setCharacterEncoding("UTF-8");
 
-    // 使用 Service 層取得所有貼文
-    List<Post> posts = postService.selectPost();
+	List<Post> posts = postService.selectPost();
+	JsonArray postsArray = new JsonArray();
 
-    // 使用 Gson 將資料轉換成 JSON 格式
-    Gson gson = new Gson();
-    String json = gson.toJson(posts);
+	for (Post post : posts) {
+		JsonObject postJson = new JsonObject();
+		postJson.addProperty("postId", post.getPostId());
+		postJson.addProperty("userId", post.getUserId());
+		postJson.addProperty("title", post.getTitle());
+		postJson.addProperty("content", post.getContent());
+		postJson.addProperty("picture",
+				post.getPicture() != null ? Base64.getEncoder().encodeToString(post.getPicture()) : null);
+		postJson.addProperty("postDate",
+				post.getPostDate() != null ? new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(post.getPostDate())
+						: null);
+		postJson.addProperty("likepost", post.getLikePost());
 
-    // 將 JSON 回傳到回應中
-    response.getWriter().write(json);
+		postsArray.add(postJson);
+	}
+
+	try (PrintWriter out = resp.getWriter()) {
+		out.println(postsArray.toString());
+		System.out.println("dataOut: " + postsArray.toString());
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
 }
+
 
 }
