6f167cb245e31d9b0f21b3da21cc1fffdfc6a637
MSI\Jay <40843245@gm.nfu.edu.tw>
2024-11-09 15:52:32 +0800

    1. add property in VO.

--- src/main/java/web/dietdiary/vo/DiaryDescriptionVO.java
+++ src/main/java/web/dietdiary/vo/DiaryDescriptionVO.java
@@ -2,6 +2,7 @@ package web.dietdiary.vo;
 
 public class DiaryDescriptionVO {
 	private int diaryID;
+	private int mealCategoryID;
 	private String foodIconUri;
 	private String foodDescription;
 	
@@ -11,6 +12,14 @@ public class DiaryDescriptionVO {
 	}
 	public void setDiaryID(int diaryID) {
 		this.diaryID = diaryID;
+	}
+	
+	
+	public int getMealCategoryID() {
+		return mealCategoryID;
+	}
+	public void setMealCategoryID(int mealCategoryID) {
+		this.mealCategoryID = mealCategoryID;
 	}
 	
 	public String getFoodIconUri() {
@@ -25,9 +34,10 @@ public class DiaryDescriptionVO {
 	public void setFoodDescription(String foodDescription) {
 		this.foodDescription = foodDescription;
 	}
+	
 	@Override
 	public String toString() {
-		return "DiaryDescriptionVO [diaryID=" + diaryID + ", foodIconUri=" + foodIconUri + ", foodDescription="
-				+ foodDescription + "]";
+		return "DiaryDescriptionVO [diaryID=" + diaryID + ", mealCategoryID=" + mealCategoryID + ", foodIconUri="
+				+ foodIconUri + ", foodDescription=" + foodDescription + "]";
 	}
 }
